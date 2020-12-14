34ea1a55eafbbb6615820126faf5e547d12eea5e
Audric-Clarence-Jovan <70675157+Audric-Clarence-Jovan@users.noreply.github.com>
2020-12-14 18:50:31 +0700

    Update users.js

--- UAS/Source Code/routes/users.js
+++ UAS/Source Code/routes/users.js
@@ -86,7 +86,7 @@ router.post("/register", (req, res) => {
 router.post('/login', (req, res, next) => {
   passport.authenticate("local",function(err, user, info) {
     if (err) { return next(err); }
-    if (!user) { return res.redirect('/login'); }
+    if (!user) { return res.render('login'); }
     req.logIn(user, function(err) {
       if (err) { return next(err); }
       req.session.name=user.name
