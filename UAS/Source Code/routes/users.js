b294495a24a9e753e05b81a6eae84fda9f383c0c
Audric-Clarence-Jovan <70675157+Audric-Clarence-Jovan@users.noreply.github.com>
2020-12-14 19:04:06 +0700

    Update users.js

--- UAS/Source Code/routes/users.js
+++ UAS/Source Code/routes/users.js
@@ -86,7 +86,12 @@ router.post("/register", (req, res) => {
 router.post('/login', (req, res, next) => {
   passport.authenticate("local",function(err, user, info) {
     if (err) { return next(err); }
-    if (!user) { return res.render('login'); }
+    if (!user) {
+      let errors = [];
+      errors.push({ msg: "kesalahan dalam penulisan email/password" });
+      return res.render('login', {
+        errors,
+          }); }
     req.logIn(user, function(err) {
       if (err) { return next(err); }
       req.session.name=user.name
